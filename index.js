0620d8296afaf87dd4699491fe4e6b44bb27a228
Samuel Wambeo <samuelwambeo9@gmail.com>
2024-04-11 00:13:40 +0300

    one entity remaining

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function displayPosts(polls)
     cardsContainer.innerHTML += `
     
 
-<div class="max-w-sm p-7 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div class="max-w-sm mx-auto p-7 w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${poll.title}</h5>
 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${poll.question}</p>
